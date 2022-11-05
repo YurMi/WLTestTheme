62ed0330391002f12aa14e96d61f64a4f99d3774
Yurii Mytrofanov <yurii@onpoint.to>
2022-11-05 20:38:40 +0200

    new style file, change title tag

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(  )?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' )?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WL Home PAge</title>
     <?php wp_head( )?>
 </head>
 
